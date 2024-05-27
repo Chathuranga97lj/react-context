d4bc3ef963ea4102dff14fcde26e0b450de177bc
Chathuranga97lj <chathuit97@gmail.com>
2024-05-27 14:59:47 +0530

    way to access context in functional component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,18 +2,23 @@ import React, {Component} from 'react';
 import { ThemeContext } from './contexts/ThemeContext';
 
 class BookList extends Component {
-    static contextType = ThemeContext;
+    // way to use functinal component
     render() {
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
         return (
-            <div className='book-list' style={{background: theme.bg, color: theme.syntax}}>
-                <ul>
-                    <li style={{background: theme.ui}}>The way of kings</li>
-                    <li style={{background: theme.ui}}>The name of the wind</li>
-                    <li style={{background: theme.ui}}>The final empire</li>
-                </ul>
-            </div>
+            <ThemeContext.Consumer>{(context) => {
+                const { isLightTheme, light, dark } = context;
+                const theme = isLightTheme ? light : dark;
+                return (
+                    <div className='book-list' style={{background: theme.bg, color: theme.syntax}}>
+                        <ul>
+                            <li style={{background: theme.ui}}>The way of kings</li>
+                            <li style={{background: theme.ui}}>The name of the wind</li>
+                            <li style={{background: theme.ui}}>The final empire</li>
+                        </ul>
+                </div>
+                )
+            }}
+            </ThemeContext.Consumer>
         );
     }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,24 @@ import React, {Component} from 'react';
 import { ThemeContext } from './contexts/ThemeContext';
 
 class Navbar extends Component {
-    // for class component usage context
-    static contextType = ThemeContext;
     render() {
-        console.log(this.context);
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
+        // way to use functional component
         return (
-            <nav style={{background: theme.ui, color: theme.syntax}}> 
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
+            <ThemeContext.Consumer>{(context) => {
+                const { isLightTheme, light, dark } = context;
+                const theme = isLightTheme ? light : dark;
+                return(
+                    <nav style={{background: theme.ui, color: theme.syntax}}>
+                    <h1>Context App</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+                )
+            }}
+            </ThemeContext.Consumer>
         );
     }
 }
